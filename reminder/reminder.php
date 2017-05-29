05732ef1d93a1eb3ec1e53d20d6b08a18911dbb7
scissormarks <scissormarks@gmail.com>
2017-05-29 21:18:09 +0200

    VERY CLOSE

--- reminder/reminder.php
+++ reminder/reminder.php
@@ -1,4 +1,4 @@
-<div id="reminder">
+<div class="reminder">
     <h2>Bliv Påmindet!</h2>
     <div>
         <p>Tilmeld dig eller en ven og få en påmindelse en uge før og på dagen, om at der er kommunalvalg den 21. november.</p>
